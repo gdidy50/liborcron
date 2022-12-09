365de581a2721af8afe78c429adf9e6cde6c1938
Grant Burke <grant.s.burke@gmail.com>
2022-12-08 22:52:24 -0600

    Extracted scraper and cron to separate files; switched to puppeteer and esm

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express')
+import express from 'express'
+import './lib/cron.js'
+
 const app = express()
 const port = 3000
-require('./lib/cron')
 
-app.get('/libor-rates', async (req, res) => {
+app.get('/libor-rates/download', async (req, res) => {
 	res.download('./libor_rates.zip')
 })
 
